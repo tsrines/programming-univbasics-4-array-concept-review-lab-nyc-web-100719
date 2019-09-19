dac00064a92c81eadaee7458f6a7d569e356e7c7
Timothy Rines <tsrines@gmail.com>
2019-09-19 14:37:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
+    # Add your solution here
 end
 
 def find_min_value(array)
